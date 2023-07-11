9d26c86209c2e6af9c3d3eeb7d65aa8daedfe3ba
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2023-07-11 11:56:59 +0200

    finish first draft navigation

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -1,16 +1,23 @@
-let hamburger = document.querySelector('.hamburger');
-let navLinks = document.getElementById('nav-links')
-let links = document.querySelectorAll('.links');
+let hamburger = document.querySelector(".hamburger");
+let navLinks = document.getElementById("nav-links")
+let links = document.querySelectorAll(".links");
+let sideBar = document.querySelector(".nav-container");
+
+var viewport = window.matchMedia("(max-width: 700px)")
 
 // Display links onClick on Hamburger
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('hide');
-    hamburger.classList.toggle('lines-rotate');
+hamburger.addEventListener("click", () => {
+    if (viewport.matches) {
+        navLinks.classList.toggle("hide");
+        hamburger.classList.toggle("lines-rotate");
+    } else {
+    sideBar.classList.toggle("toggle-function");
+    }
 });
 
 // Hide navLink Container onClick any single link
 for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', () => {
-        navLinks.classList.toggle('hide');
+    links[i].addEventListener("click", () => {
+        navLinks.classList.toggle("hide");
     });
 }
