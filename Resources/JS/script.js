45ff55dea9f1040299184e3be22702e2c78f4c4d
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2023-07-12 23:41:58 +0200

    add logo and refined navigation with hover effect and changed dropdown navigation on mobile displays

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -1,17 +1,16 @@
 let hamburger = document.querySelector(".hamburger");
 let navLinks = document.getElementById("nav-links")
 let links = document.querySelectorAll(".links");
-let sideBar = document.querySelector(".nav-container");
+let menu = document.querySelector(".nav-container");
 
 var viewport = window.matchMedia("(max-width: 700px)")
 
 // Display links onClick on Hamburger
 hamburger.addEventListener("click", () => {
     if (viewport.matches) {
-        navLinks.classList.toggle("hide");
-        hamburger.classList.toggle("lines-rotate");
+        menu.classList.toggle("toggle-function-dd");
     } else {
-    sideBar.classList.toggle("toggle-function");
+    menu.classList.toggle("toggle-function-sb");
     }
 });
 
