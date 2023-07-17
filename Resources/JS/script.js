91d65a0f83d373cc13bc2ee09805166c664c1bf8
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2023-07-17 22:57:38 +0200

    correct social link linkedin and try change hamburger symbol

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -1,13 +1,55 @@
 let hamburger = document.querySelector(".hamburger");
+let lines = hamburger.querySelector(".lines")
+let cross = hamburger.querySelector(".lines")
 let menu = document.querySelector(".nav-container");
+let logo = document.querySelector(".logo");
+let navLinks = document.querySelector(".nav-links");
+let socialMedia = document.querySelector(".social-media-icons")
+const menuElements = [menu, logo, navLinks, socialMedia];
 
 var viewport = window.matchMedia("(max-width: 650px)")
 
-// Display links onClick on Hamburger
+// Display menu onClick on Hamburger
 hamburger.addEventListener("click", () => {
+    if (viewport.matches) {
+        menu.classList.toggle("toggle-function-dd");
+        lines.style.display = "none";
+        cross.style.display = "block";
+        
+    } else {
+        menu.classList.toggle("toggle-function-sb");
+        lines.style.display = "none";
+        cross.style.display = "block";
+    }
+});
+
+//close menu onClick on Social media
+/* socialMedia.addEventListener("click", () => {
     if (viewport.matches) {
         menu.classList.toggle("toggle-function-dd");
     } else {
     menu.classList.toggle("toggle-function-sb");
     }
+}); */
+
+//close menu onClick on nav links
+/* navLinksLinks.addEventListener("click", () => {
+    if (viewport.matches) {
+        menu.classList.toggle("toggle-function-dd");
+    } else {
+    menu.classList.toggle("toggle-function-sb");
+    }
+}); */
+
+//close menu onClick everywhere 
+document.addEventListener('click', function handleClickOutsideMenu(event) {
+    const cl = menu.classList;
+    const clContain = cl.contains
+        if (!menu.contains(event.target)) {
+            if (viewport.matches) {
+                menu.classList.toggle("toggle-function-dd");
+            } else {
+            menu.classList.toggle("toggle-function-sb");
+            }
+        }
 });
