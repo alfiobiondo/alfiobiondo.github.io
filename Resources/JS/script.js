d2732afb284c769cd893622259639c5a02cc6187
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2023-07-18 01:19:04 +0200

    resolve problem javascript change icon burger menu to cross and close menu when click everywere outside it

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -1,6 +1,6 @@
 let hamburger = document.querySelector(".hamburger");
-let icon = hamburger.querySelector(".fa-bar")
 let menu = document.querySelector(".nav-container");
+let icon = document.querySelector(".fa-bars");
 let logo = document.querySelector(".logo");
 let navLinks = document.querySelector(".nav-links");
 let socialMedia = document.querySelector(".social-media-icons")
@@ -9,50 +9,52 @@ const menuElements = [menu, logo, navLinks, socialMedia];
 var viewport = window.matchMedia("(max-width: 650px)")
 
 // Display menu onClick on Hamburger
-icon.addEventListener("click", () => {
-    if (icon.classList.contains("fa-bars")) {
-        icon.classList.replace("fa-bars", "fa-xmark");
-    } else {
-        icon.classList.replace("fa-times", "fa-xmark");
-    }
-});
-
-// Display menu onClick on Hamburger
 hamburger.addEventListener("click", () => {
     if (viewport.matches) {
-        menu.classList.toggle("toggle-function-dd");      
+        menu.classList.toggle("toggle-function-dd");     
     } else {
         menu.classList.toggle("toggle-function-sb");
     };
 });
 
+hamburger.onclick = () => {
+    if (icon.classList.contains("fa-bars")) {
+        icon.classList.replace("fa-bars", "fa-xmark");
+    }
+    else {
+        icon.classList.replace("fa-xmark", "fa-bars");
+    }
+};
+
 //close menu onClick on Social media
-/* socialMedia.addEventListener("click", () => {
+socialMedia.addEventListener("click", () => {
     if (viewport.matches) {
         menu.classList.toggle("toggle-function-dd");
     } else {
-    menu.classList.toggle("toggle-function-sb");
+        menu.classList.toggle("toggle-function-sb");
     }
-}); */
+});
 
 //close menu onClick on nav links
-/* navLinksLinks.addEventListener("click", () => {
+/* navLinks.addEventListener("click", () => {
     if (viewport.matches) {
         menu.classList.toggle("toggle-function-dd");
     } else {
-    menu.classList.toggle("toggle-function-sb");
+        menu.classList.toggle("toggle-function-sb");
     }
 }); */
 
-//close menu onClick everywhere 
+//close menu onClick everywhere if menu open
 document.addEventListener('click', function handleClickOutsideMenu(event) {
-    const cl = menu.classList;
-    const clContain = cl.contains
+    if (icon.classList.contains("fa-xmark")) {
         if (!menu.contains(event.target)) {
             if (viewport.matches) {
                 menu.classList.toggle("toggle-function-dd");
+                icon.classList.replace("fa-xmark", "fa-bars");
             } else {
-            menu.classList.toggle("toggle-function-sb");
+                menu.classList.toggle("toggle-function-sb");
+                icon.classList.replace("fa-xmark", "fa-bars");
             }
         }
+    }
 });
